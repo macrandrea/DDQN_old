a3fa506fc6def2cf72b968b91f60a1408d160751
macrandrea <58860052+macrandrea@users.noreply.github.com>
2023-01-20 15:04:06 +0100

    ThirdCommit_20_01_23
    Class Trader depleted -> substituted by Class Agente
    Implementato:
    - sconto epsilon
    - Q-function per periodi 4 e 5
    - Migliorata funzione di reward
    -Update Target Q-function
    To do:
    -train su più cammini MC
    -funzione di test
    -A&C per comparare

--- Agente.py
+++ Agente.py
@@ -13,230 +13,142 @@ from Ambiente import Ambiente
 
 env = Ambiente()
 
-Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
-State = namedtuple("State", "inventory, time, qdr_var, price")
-Action = namedtuple("Action", "amount_sold")
-
-
 class ReplayMemory():
 
     def __init__(self, capacity):
+
         self.memory = deque([], maxlen=capacity)
 
-    def add(self, state, action, next_state, reward):
-        self.memory.append(Transition(state, action, next_state, reward))
+    def add(self,inv, time, price, var, x, next_inv, next_time, next_price, next_var, x_new, reward): #inv, time, price, var, x, next_state, reward state, action, next_state, reward
+        
+        self.memory.append([inv, time, price, var, x, next_inv, next_time, next_price, next_var, x_new, reward])
 
     def sample(self, batch_size):
+        
         return rnd.sample(self.memory, batch_size)
 
     def __len__(self):
+        
         return len(self.memory)
 
 class Agente():
 
     def __init__(self):
+
         self.q_net = self._build_dqn_model()
         self.target_q_net = self._build_dqn_model()
         self.time_subdivisions = 5
-        self.starting_trading_time = 0
-        self.time = 0
-        self.inventory = 20
-        self.amount_sold = 0
+        self.inventory = 100
         self.a_penalty = 0.0001
         self.epsilon = 0.5
+        self.epsilon_decay = 0.995
         self.passo = 1200
-        self.batch_size = 64
+        self.batch_size = 128
         self.gamma = 0.95
-        self.memory = ReplayMemory(500)
-
+        self.memory = ReplayMemory(5000)
+    
     @staticmethod
     def _build_dqn_model():
-        """
-        Builds a deep neural net which predicts the Q values for all possible
-        actions given a state. The input should have the shape of the state, and
-        the output should have the same shape as the action space since we want
-        1 Q value per possible action.
 
-        :return: Q network
-        """
         q_net = Sequential()
-        q_net.add(tf.keras.layers.Input(shape=(4,))) #4 = [inventory, time, price, stddev] -> state
-        q_net.add(Dense(64, activation = 'relu'  , kernel_initializer = 'he_uniform')) #Draws samples from a uniform distribution within
-        q_net.add(Dense(32, activation = 'relu'  , kernel_initializer = 'he_uniform'))
-        q_net.add(Dense(1 , activation = 'linear', kernel_initializer = 'he_uniform'))
+        q_net.add(tf.keras.layers.Input(shape=(5,))) #4 = [inventory, time, price, stddev, prev_action, state]
+        q_net.add(Dense(128, activation = 'relu'  )) 
+        q_net.add(Dense(128, activation = 'relu'  ))
+        q_net.add(Dense(64, activation = 'relu'  )) 
+        q_net.add(Dense(64, activation = 'relu'  ))
+        q_net.add(Dense(32, activation = 'relu'  ))      
+        q_net.add(Dense(1 , activation = 'linear')) # esce un vettore di q values per ogni azione 
         q_net.compile(optimizer = tf.optimizers.Adam(learning_rate = 0.001), loss = 'mse')
 
         return q_net
 
-    def step(self, curr_state, data, x, min_p, max_p, min_v, max_v):
+    def action(self, state, x, i):
 
-        time_step = curr_state.time# + 1
-        x = self.collect_policy(curr_state, x, min_p, max_p, min_v, max_v)
-        reward = self.calc_reward(curr_state, x, data)
-        price = env.returns(data)
-        var = env.var(data)
-        next_state = State(curr_state.inventory - x.amount_sold, time_step + 1, price[time_step], var[time_step])
+        if state[0] == 0:
+            return 0
+        elif i == 5:
+            return state[0]
+        elif np.random.rand() < self.epsilon:
+            rand_act = np.random.binomial(state[0], 1 / (self.time_subdivisions))#- t
+            self.epsilon *= self.epsilon_decay
+            return rand_act
+        else:
+            state.append(x)
+            action = np.argmax(self.q_net(np.array([state]).astype('float32')))
 
-        self.memory.add(curr_state, x, next_state, reward)
+        return action
+
+    def rew(self, state, action, reward):
+
+        #reward = 0 #mh
+        if state[0] == 0:
+            reward == 0
+            return reward
+        else:
+            xs = action
+            reward = state[0] * (state[2]) - self.a_penalty * (xs ** 2)
+
+            return reward
+
+    def reset(self, data, act, i):
+
+        price = Ambiente().returns(data[0:2])
+        var   = Ambiente().var(price[0:2])
+        if i == 1:
+            action =  rnd.uniform(0, 1)
+        else:
+            action = act
+
+        return self.inventory, 0, price[0], var, action #rnd.uniform(0, 1)
+
+    def step(self, inv, time, price, var,  x, i):
+        reward = 0
+        state = [inv, time , price[time], var[time]]
+        x_new = self.action(state, x, i)
+        reward = self.rew(state, x_new, reward)
+        next_state = [inv - x_new, time + 1 , price[time- 1], var[time- 1]]
+        self.memory.add(inv, time, price[time], var[time], x, inv - x_new, time, price[time], var[time], x_new, reward)
 
         if len(self.memory) < self.batch_size:
 
-            return 1, 0, reward, next_state, self.epsilon
+            return 1, reward, next_state, x_new, self.epsilon#0,
 
         else:
 
             transitions = self.memory.sample(self.batch_size)
+            loss, grad = self.train(transitions, i)
 
-        return *self.train(transitions, data), reward, next_state, self.epsilon
+            return loss, reward, next_state, x_new, self.epsilon
 
-    def reset(self, data):
+    def train(self, trans, i):
 
-        price = Ambiente().returns(data[0:2])
-        var   = Ambiente().var(price[0:2])
+        batch = np.asarray(trans).astype('float32')
+        state_act_batch      = batch[:,:5]
+        next_state_act_batch = batch[:, 5: 10]
+        reward_batch     = batch[:, 10]
+        q_val = self.q_net(state_act_batch)#self.q_net.predict(state_batch)
+        q_next = self.target_q_net(next_state_act_batch)#self.target_q_net.predict(next_state_batch)
+        if i < 4:
+            q_val = reward_batch + self.gamma * np.max(q_next)
 
-        return State(self.inventory, 0, 0.1 , price), Action(rnd.uniform(0, 1))#var
+            training = self.q_net.fit(state_act_batch, q_val, epochs=100, verbose=0)
+            loss = training.history['loss']
+        elif i == 4:
+            correction_term = (next_state_act_batch[0][0]) * (next_state_act_batch[0][2]) - self.a_penalty * ((next_state_act_batch[0][0]) ** 2)
 
-    def collect_policy(self, state, x, min_p, max_p, min_v, max_v):
+            q_val = reward_batch + self.gamma * correction_term
 
-        q = state.inventory
-        t = state.time
+            training = self.q_net.fit(state_act_batch, q_val, epochs=100, verbose=0)
+            loss = training.history['loss']
+        else:
+            q_val = reward_batch
 
-        if q == 0:
+            training = self.q_net.fit(state_act_batch, q_val, epochs=100, verbose=0)
+            loss = training.history['loss']
+        grad_norm = 0
+        #update della rete q
 
-            return Action(0)
+        if state_act_batch[1][0]%20 == 0:
+            q_next = q_val
 
-        if self.epsilon > rnd.uniform(0, 1):
-
-            rand_act = np.random.binomial(q, 1 / (self.passo - t))
-
-            return Action(rand_act)
-        else: 
-
-            x = self.choose_best_action(state, x, min_p, max_p, min_v, max_v)
-            
-            return x
-    
-    def choose_best_action(self, state, x, min_p, max_p, min_v, max_v):
-
-        q, t, price, var, x = env.normalise(state.inventory, state.time, state.price, state.qdr_var, x, min_p, max_p, min_v, max_v ) 
-        state = State(q, t, price, var) 
-        input = tf.convert_to_tensor(np.asarray(state).reshape(1,-1).astype('float32'))#, dtype=tf.float32
-        action_q = self.q_net(input)
-        action = Action(np.argmax(action_q.numpy()[0], axis=0))
-
-        return action
-    
-    def calc_reward(self, state, action, data_set):
-        # da cambiare, in ottica che io già divido in 5 intervalli, il reward per ogni intervallo deve essere considerato.
-        # al posto di data set i+1 - dataset i io darei direttamente il price, è già il ritorno
-        # x sembrerebbe essere già per ogni secondo perchè è considerato in tesp che ti da il ritorno per due intervalli consecutivi
-        ##########################
-        # è sbagliato forse, per ogni azione va un reward, qui fa per una sola molti reward -> cambia!!!!!!!!!!!!!!!!!!
-        ##########################
-        reward = 0
-        M = self.time_subdivisions
-        t = state.time
-        q = state.inventory
-        x = action.amount_sold
-        a = self.a_penalty
-        xs = x / M  # amount sold each second
-        T_0 = self.starting_trading_time        
-        inventory_left = q
-        for i in range(T_0 + M * t, T_0 + M * (t + 1)):
-
-            if i + 1 < len(data_set):
-                reward += inventory_left * (data_set[i + 1] - data_set[i]) - a * (xs ** 2)
-                inventory_left -= xs
-
-        return reward
-
-    def call_tensor_q_net(self,state_batch):
-        
-
-        return self.q_net(tensor)
-    def train(self, transitition, data):
-
-        batch = Transition(*zip(*transitition))
-    # PROBLEMA : DONE E' TRUE QUANDO SONO AD INTERVALLO 5, GLIELO DEVO DIRE E PASSARE L'INFO NEI METODI 
-        state_batch, action_batch, next_state_batch, reward_batch = batch #("state", "action", "next_state", "reward"), done_batch
-        current_q  = self.q_net(np.array(state_batch).astype('float32'))#.numpy() # non funziona con batch - > ordinali in un tensore [64,4,1]
-        #mi dice setting an array element with a sequence -> come risolvere??
-        #crea una funzione che ti ridia indietro current_q
-        target_q   = np.copy(current_q)
-        next_q     = self.target_q_net(next_state_batch).numpy()
-        max_next_q = np.amax(next_q, axis=1)
-
-        for i in range(state_batch.shape[0]):
-
-            target_q_val = reward_batch[i]
-            
-            if not done_batch[i]: #
-            
-                target_q_val += reward_batch[i] + self.gamma * max_next_q[i] #gamma = 0.95
-            
-            target_q[i][action_batch[i]] = target_q_val
-        
-        training_history = self.q_net.fit(x=state_batch, y=target_q, verbose=0)
-
-        loss = training_history.history['loss']
-        #computing the loss, it must be 
-            #target = r + self.gamma * Qp_eval
-            #loss = torch.mean(( target.detach() - Q_eval )**2)
-        return loss
-
-
-        '''
-            def evaluate_Q(self, current_state, current_action, type_input, net='main_net'):
-
-        if type_input == 'scalar':
-            # print(current_state.inventory, current_state.time, current_action.amount_sold)
-            q, t, qdr_var, price, x = self.normalization(current_state.inventory,
-                                                         current_state.time,
-                                                         current_state.qdr_var,
-                                                         current_state.price,
-                                                         current_action.amount_sold
-                                                         )
-
-            in_features = torch.tensor(
-                [
-                    q,
-                    t,
-                    qdr_var,
-                    price,
-                    x
-                ],
-                dtype=torch.float
-            )
-
-        elif type_input == 'tensor':
-            in_features = []
-            for (current_state, current_action) in zip(current_state, current_action):
-                q, t, qdr_var, price, x = self.normalization(current_state.inventory,
-                                                             current_state.time,
-                                                             current_state.qdr_var,
-                                                             current_state.price,
-                                                             current_action.amount_sold
-                                                             )
-
-                in_features.append(torch.tensor(
-                    [
-                        q,
-                        t,
-                        qdr_var,
-                        price,
-                        x,
-                    ],
-                    dtype=torch.float
-                )
-                )
-
-            in_features = torch.stack(in_features)
-            in_features.type(torch.float64)
-
-        if net == 'main_net':
-            return self.main_net(in_features).type(torch.float64)
-        elif net == 'target_net':
-            return self.target_net(in_features).type(torch.float64)
-        
-        
-        '''
+        return loss, grad_norm
--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
-from Trader import Agente
-#from Agente_pytorch import Agente
-from Trader import ReplayMemory
+from Agente import Agente
+from Agente import ReplayMemory
+import random as rnd
+#from Trader import Agente
+#from Trader import ReplayMemory
 from Ambiente import Ambiente
 import numpy as np
 import math as m
@@ -39,14 +41,6 @@ def matriciIntervalli(data,numSlice):
 
     return price, var, dati
 
-#def main():
-#    agent = Agente()
-#    #cicli
-#    #chiama step
-#    #salva le loss ecc
-#
-#    reward_history, epsilon_history, grad_norm_history, loss_history, qdr_var_history, price_history = doTrain(agent)
-#    return reward_history, epsilon_history, grad_norm_history, loss_history, qdr_var_history, price_history
 
 #if __name__ == "main":
 numItTrain = 1
@@ -59,63 +53,28 @@ loss_history = []
 
 data = env.gbm().flatten()
 pri, v, datiSli = matriciIntervalli(data, numSlice=numSlice)
-state = age.reset(datiSli)
+#state = age.reset(datiSli)
+action = rnd.uniform(0, 1)
 
-for i in tqdm(range(numSlice - 1)):
+for i in tqdm(range(numSlice - 1)):# quanti periodi dividi la giornata, in questo caso 5
 
-    inv, time, price, var, action = age.reset(datiSli[i, :])
+    inv, time, price, var, action = age.reset(datiSli[i, :], action, i) # questo pezzo va messo fuori dai loop
     min_p, max_p, min_v, max_v = pri[i, :].min(), pri[i, :].max(), v[i, :].min(), v[i, :].max()
     reward_episode = 0
 
-    for ii in tqdm(range(numItTrain)):
+    for ii in tqdm(range(numItTrain)):# quante volte vuoi fare il train
 
-        for iii in tqdm(range(len(datiSli[i, :]))):
+        for iii in tqdm(range(2 ,(len(datiSli[i, :])) + 1)): # per ogni 1200 time steps fai questo
 
-            (loss, grad_norm, reward, next_state, epsilon) = age.step(inv, time, price, var, datiSli[i, :], action, min_p, max_p, min_v, max_v)#
+            (loss, reward, next_state, x_new, epsilon) = age.step(inv, iii-2, pri[i], v[i], action, i)#datiSli[i, :], min_p, max_p, min_v, max_v
+            inv, time, action = next_state[0], next_state[1], x_new
             current_state = next_state
             # modifica anche la funzione step -> gli devi far capire che siamo in 5, e se siamo in 6 deve assolutamente liquidare tutto quanto!
 
             reward_episode += reward
         loss_history.append(loss)
         epsilon_history.append(epsilon)
-        reward_history.append(reward_episode)
+        reward_history.append(reward_episode/1200)
         #grad_norm_history.append(grad_norm)m.log(loss)
 
 print(reward_history, loss_history)
-'''    
-
-for t in range(numTraj):# ciclo di quante traiettorie vuoi considerare
-    data = Ambiente().gbm().flatten()
-    dati = sliceData(data, numSlice)
-    price= np.empty((numSlice, int(len(data) /numSlice) -1))
-    var  = np.empty((numSlice, int(len(data)/numSlice) -1))
-    for i in range(dati.shape[0]): # divido per 5 intervalli
-        price[i,:] = Ambiente().returns(dati[i,:])
-        var  [i,:] = Ambiente().var(dati[i,:])
-        #for j in range(numItTrain): # epochs di train
-
-
-
-
-'''
-    #price = Environment().returns(data)
-    #var   = Environment().var(data)
-    #for i in range(data.shape[1]):# per ogni traiettoria MC
-    #    state, x = Agente().reset()
-    #    rewards = []
-    #    for ii in range(numItTrain):# numero di volte in cui fai il train: epochs
-    #        # questo va fatto sui 5 sotto insiemi di data
-    #        loss, reward, next_state, epsilon = Agente().step(state, data)
-    #        state = next_state
-    #        rewards.append(reward)
-# capire come spezzare in 5 intervalli i dati
-    #for i in range()
-
-    #reward_history, epsilon_history, grad_norm_history, loss_history, qdr_var_history, price_history = main()
-
-
-#stock_dict = {"train_drift": 6000}
-#stock_dict_pre_training = {"train_drift": 6000}
-#for stock in tqdm(stock_dict.keys()):
-#    for i in tqdm(range(stock_dict[stock])):
-#        print('a')
